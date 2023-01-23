1346a86799fbac79d42915ab9bb40bf9cd827647
njoro410 <brian.njoroge@student.moringaschool.com>
2023-01-23 15:53:31 +0300

    DB:added database variables

--- backend/settings.py
+++ backend/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '2yU9P2VqSniW85FRrnjZ',
+        'HOST': 'containers-us-west-53.railway.app',
+        'PORT': '7758',
     }
 }
+
 
 
 # Password validation
