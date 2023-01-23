6cf7ecf80bff399af21a2b6b1c3c23b84e4d9456
sharonkorir <sharon.korir@student.moringaschool.com>
2023-01-23 17:29:59 +0300

    Database: rename members and residential areas tables

--- members/models.py
+++ members/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class ResidentialAreas(models.Model):
+class Residential_Areas(models.Model):
   area_code = models.IntegerField(primary_key=True)
   name = models.CharField(max_length=120)
 
@@ -10,12 +10,12 @@ class ResidentialAreas(models.Model):
   def __str__(self):
       return self.name
 
-class Member(models.Model):
+class Members(models.Model):
   names = models.CharField(max_length=120, null = True, blank=True)
   mbr_no = models.IntegerField()
   id_no = models.IntegerField(blank=True, null=True)
   gender = models.CharField(null = True, blank=True, max_length=120)
-  residential = models.ForeignKey(ResidentialAreas, on_delete=models.DO_NOTHING, null=True, blank=True)
+  residential = models.ForeignKey(Residential_Areas, on_delete=models.DO_NOTHING, null=True, blank=True)
   phone_no = models.CharField(blank=True, null=True, max_length=120)
   next_of_kin = models.CharField(max_length=120, null=True, blank=True)
   phone_nos = models.CharField(blank=True, null=True, max_length=120)
