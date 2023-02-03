f441f4eafefbf5a0ced8f23d0d891e4c18447911
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-03 14:26:24 +0300

    Feat: added view function to get and post loan types

--- loans/views.py
+++ loans/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Loans, Loan_Type, Documents, documentType
+from .serializers import LoanTypeSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def loantypes(request):
+    """
+    list and add loan types
+    """
+    if request.method == 'GET':
+        types = Loan_Type.objects.all()
+        serializer = LoanTypeSerializer(types, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = LoanTypeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
