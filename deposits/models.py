255c0ff1c00029d762ba76b189f1aefcdc98ed9e
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-03 12:09:25 +0300

    DB: created models for deposit account app

--- deposits/models.py
+++ deposits/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from members.models import Members
+from administration.models import baseModel
 
 # Create your models here.
+class Deposits_Account(baseModel):
+    account_owner = models.ForeignKey(Members, on_delete=models.CASCADE, related_name='deposits_account')  
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
+    
+    
+    def __str__(self):
+        return self.account_owner.names
+    
