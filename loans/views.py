6c4e5a9a309220daf874642b93326b933f0149ac
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-04 11:11:01 +0300

    Feat:added a view function to get all loans

--- loans/views.py
+++ loans/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Loans, Loan_Type, Documents, documentType
-from .serializers import LoanTypeSerializer
+from .serializers import LoanTypeSerializer, LoanSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,7 +8,7 @@ from rest_framework import status
 # Create your views here.
 
 
-@api_view(['GET', 'POST' ])
+@api_view(['GET', 'POST'])
 def loan_types_create_add(request):
     """
     function to list and add loan types
@@ -17,17 +17,17 @@ def loan_types_create_add(request):
         types = Loan_Type.objects.all()
         serializer = LoanTypeSerializer(types, many=True)
         return Response(serializer.data)
-    
+
     elif request.method == 'POST':
         serializer = LoanTypeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
+
+
 @api_view(['GET', 'PUT', 'DELETE'])
-def loantype_detail(request,id):
+def loantype_detail(request, id):
     """
     Retrieve, update or delete a loan type.
     """
@@ -50,7 +50,10 @@ def loantype_detail(request,id):
     elif request.method == 'DELETE':
         loantype.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    
-    
-    
+
+
+@api_view(['GET'])
+def get_all_loans(request):
+    loans = Loans.objects.all()
+    serializer = LoanSerializer(loans, many=True)
+    return Response(serializer.data)
