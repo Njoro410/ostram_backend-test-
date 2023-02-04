0c035b1f7375f01c8dffd112d6959e2af05a0b8d
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-04 16:36:34 +0300

    Feat:added a view function to get total savings

--- savings/views.py
+++ savings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import SavingsSerializer
+from rest_framework import status
+from .models import Savings_Account
+from django.db.models import Sum
 # Create your views here.
+
+@api_view(['GET'])
+def get_total_savings(request):
+    total_savings = Savings_Account.objects.all().aggregate(Sum('balance'))['balance__sum']
+    return Response({'total_savings': total_savings}, status=status.HTTP_200_OK)
