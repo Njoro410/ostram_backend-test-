5efc309abbb0d8121b447f9611763baea5a67751
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-04 16:34:42 +0300

    added savings urls to main project urls

--- backend/urls.py
+++ backend/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('loans/',include('loans.urls'))
+    path('loans/',include('loans.urls')),
+    path('savings/',include('savings.urls')),
 ]
