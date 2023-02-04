d8bda6a79461da6b8ad056455f988dfb92dab137
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-04 10:09:40 +0300

    URLS: Added a loantype details url

--- loans/urls.py
+++ loans/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('loantypes/',views.loantypes, name='loantypes'),
+    path('loantypes/',views.loan_types_create_add, name='loantypes'),
+    path('loantypes/details/<int:id>/',views.loantype_detail, name='loantype_details'),
 ]
