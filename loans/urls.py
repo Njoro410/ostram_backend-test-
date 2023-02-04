8ed9b9592fb2653cafceba45296ec823ff78090a
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-04 11:11:27 +0300

    Feat:added a url for getting all loans

--- loans/urls.py
+++ loans/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('loantypes/',views.loan_types_create_add, name='loantypes'),
     path('loantypes/details/<int:id>/',views.loantype_detail, name='loantype_details'),
+    path('all_loans/',views.get_all_loans, name='loans'),
 ]
