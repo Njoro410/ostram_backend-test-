936aad1293e806069659bc30e35d10232e456ff0
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-04 16:36:57 +0300

    added a url that gets loans by member no

--- loans/urls.py
+++ loans/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('loantypes/',views.loan_types_create_add, name='loantypes'),
     path('loantypes/details/<int:id>/',views.loantype_detail, name='loantype_details'),
     path('all_loans/',views.get_all_loans, name='loans'),
+    path('member_loans/<int:member_id>/',views.get_loans_by_member_id, name='loans_by_member_id'),
 ]
