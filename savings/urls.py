5e01f1ebd91023e2892e25c2f38941de209b09e5
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-05 14:14:32 +0300

    added a url to get a members savings

--- savings/urls.py
+++ savings/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('total_savings/',views.get_total_savings,name='total_savings')
+    path('total_savings/',views.get_total_savings,name='total_savings'),
+    path('member_savings/<int:member_no>/',views.get_member_savings,name='member_savings'),
 ]
