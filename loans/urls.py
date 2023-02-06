bd1d545f5eb0632c882f8ff30a113d7e40240c25
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-06 12:00:29 +0300

    renamed and added url for getting loan documents

--- loans/urls.py
+++ loans/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('loantypes/',views.loan_types_create_add, name='loantypes'),
+    path('loantypes/',views.loan_types_get_add, name='loantypes'),
     path('loantypes/details/<int:id>/',views.loantype_detail, name='loantype_details'),
     path('all_loans/',views.get_all_loans, name='loans'),
-    path('member_loans/<int:member_id>/',views.get_loans_by_member_id, name='loans_by_member_id'),
+    path('member_loans/<int:member_no>/',views.get_loans_by_member_no, name='loans_by_member_no'),
+    path('loan_documents/<int:loan_id>/',views.loan_documents_by_loan_id, name='loan_documents')
 ]
