ecdc58c068f7779e9b4ccc5e0505a0dcb88fb90e
sharonkorir <sharon.korir@student.moringaschool.com>
2023-02-06 08:40:56 +0300

    APIs: seperate view and create member endpoints

--- members/urls.py
+++ members/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import MemberList, MemberDetail, ResidentialAreaList, ResidentialAreaDetail
 
 urlpatterns = [
-  path('', MemberList.as_view()),
+  path('view/', MemberList.as_view()),
+  path('create/', MemberList.as_view()),
   path('member/<int:pk>/', MemberDetail.as_view()),
   path('residential-areas/', ResidentialAreaList.as_view()),
   path('residential-areas/<int:pk>/', ResidentialAreaDetail.as_view()),
