7e64f8332b9323fa82092992b185a38a0067346e
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-07 22:10:07 +0300

    added appropriate imports

--- assetmanager/views.py
+++ assetmanager/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Asset,assetDocument
+from .serializers import AssetSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+
+# @api_view(['GET', 'POST', 'PUT', 'DELETE'])
+# def 
