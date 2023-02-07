2e373c742c284c0672ba16f1d5fa92645221e373
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-07 22:09:02 +0300

    Database:added a class meta for specifying table names

--- administration/models.py
+++ administration/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from members.models import Residential_Areas
+from members.models import residential_areas
 from django.contrib.auth.models import User
 from .choices import *
 from phonenumber_field.modelfields import PhoneNumberField
@@ -20,20 +20,27 @@ class baseModel(models.Model):
         self.created_by = request.user
         self.branch = request.user.person.branch
 
+    class Meta:
+        db_table = "base_model"
+
 
 class Branch(models.Model):
     full_address = models.CharField(max_length=200, null=True, blank=True)
-    location = models.ForeignKey(Residential_Areas, on_delete=models.CASCADE)
+    location = models.ForeignKey(residential_areas, on_delete=models.CASCADE)
     phone = PhoneNumberField(blank=True, null=True)
     email = models.EmailField(null=True, blank=True)
     manager = models.ForeignKey(
         User, on_delete=models.CASCADE, blank=True, related_name='branch_manager')
     created_by = models.ForeignKey(
         User, on_delete=models.CASCADE, blank=True, related_name='created_by_on_branch')
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
     updated_by = models.ForeignKey(
         User, on_delete=models.CASCADE, blank=True, related_name='updated_by_branch')
+    
+    
+    class Meta:
+        db_table = "branch"
 
     def __str__(self):
         return self.location.name
@@ -59,6 +66,26 @@ class Staff(baseModel):
         max_length=200, choices=EDUCATIONAL_STATUS_CHOICES, blank=True)
     manager = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='manager', blank=True, null=True)
+    
+    class Meta:
+        db_table = "staff"
 
     def __str__(self):
         return self.fullname
+
+
+class globalCharges(models.Model):
+    name = models.CharField(max_length=50)
+    maintenance_fee = models.DecimalField(max_digits=5, decimal_places=2)
+    late_charges = models.DecimalField(max_digits=5, decimal_places=2)
+    loan_form = models.DecimalField(max_digits=5, decimal_places=2)
+    general_charges = models.DecimalField(max_digits=5, decimal_places=2)
+    affidavit_fee = models.DecimalField(max_digits=5, decimal_places=2)
+    passbook = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    class Meta:
+        db_table = "global_charges"
+    
+    def __str__(self):
+        return self.name
+    
