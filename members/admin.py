1b75407125f95ee2420feb2f32c52583539c0628
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-07 22:16:20 +0300

    renamed imports

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Members)
-admin.site.register(Residential_Areas)
+admin.site.register(members)
+admin.site.register(residential_areas)
