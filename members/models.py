284a4821592ff0034bb48c20c251aa5ab757512f
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-07 22:18:12 +0300

    created class meta for specialized db table name

--- members/models.py
+++ members/models.py
@@ -1,28 +1,34 @@
 from django.db import models
-
+from administration.choices import *
 # Create your models here.
 
-class Residential_Areas(models.Model):
+class residential_areas(models.Model):
   area_code = models.IntegerField(primary_key=True)
-  name = models.CharField(max_length=120)
+  name = models.CharField(max_length=255, blank=True)
 
-
+  class Meta:
+        db_table = "residential_areas"
+        
   def __str__(self):
       return self.name
+    
 
-class Members(models.Model):
-  names = models.CharField(max_length=120, null = True, blank=True)
-  mbr_no = models.IntegerField()
+
+class members(models.Model):
+  names = models.CharField(max_length=255, null = True, blank=True)
+  mbr_no = models.IntegerField(primary_key=True)
   id_no = models.IntegerField(blank=True, null=True)
-  gender = models.CharField(null = True, blank=True, max_length=120)
-  residential = models.ForeignKey(Residential_Areas, on_delete=models.DO_NOTHING, null=True, blank=True)
-  phone_no = models.CharField(blank=True, null=True, max_length=120)
-  next_of_kin = models.CharField(max_length=120, null=True, blank=True)
-  phone_nos = models.CharField(blank=True, null=True, max_length=120)
-  relationship = models.CharField(max_length=120, null = True, blank=True)
+  gender = models.CharField(null = True, blank=True, max_length=255, choices=GENDER_CHOICES)
+  residential = models.ForeignKey(residential_areas, on_delete=models.DO_NOTHING, null=True, blank=True)
+  phone_no = models.CharField(blank=True, null=True, max_length=255)
+  next_of_kin = models.CharField(max_length=255, null=True, blank=True)
+  phone_nos = models.CharField(blank=True, null=True, max_length=255)
+  relationship = models.CharField(max_length=255, null = True, blank=True)
   image = models.ImageField(upload_to='members/passport_photo/', blank=True, null=True)
   
-
+  class Meta:
+        db_table = "members"
+        
   def __str__(self):
       return self.names
 
