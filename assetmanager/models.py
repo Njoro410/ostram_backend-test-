df1d34a170d8d4788125b7307f6e342fd414d164
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-09 13:40:53 +0300

    Delete:removed asset document model

--- assetmanager/models.py
+++ assetmanager/models.py
@@ -2,49 +2,42 @@ from django.db import models
 from administration.choices import *
 from administration.models import baseModel
 from members.models import members
+from loans.models import Loans
 from administration.models import Branch
+
 
 # Create your models here.
 
 
-class Asset(baseModel):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    expiry_date = models.DateField(null=True)
-    inspection_date = models.DateField(null=True)
-    owner = models.ForeignKey(members, on_delete=models.CASCADE,)
+class loanAsset(baseModel):
+    loan = models.ForeignKey(Loans, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=True)
+    description = models.TextField(null=True)
+    file = models.FileField(upload_to='assetdocuments/',null=True)
+    expiry_date = models.DateField(null=True,blank=True)
+    inspection_date = models.DateField(null=True,blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, null=True)
     value = models.IntegerField(null=True)
 
     class Meta:
-        db_table = "asset"
+        db_table = "loan_asset"
 
     def __str__(self):
-    	return self.name
- 
-class officeAsset(Asset):
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True,blank=True)
-    class Meta:
-         db_table = "office_asset"
-         
-    def __str__(self):
-        return f"{branch.location.name}'s office asset"
+    	return f"{self.loan.lendee.names}'s {self.name}"
  
 
 
+# class assetDocument(baseModel):
+#     upload_date = models.DateField(auto_now_add=True)
+#     document_name = models.CharField(max_length=200, null=True, blank=True)
 
-
-class assetDocument(baseModel):
-    upload_date = models.DateField(auto_now_add=True)
-    document_name = models.CharField(max_length=200, null=True, blank=True)
-    file = models.FileField(upload_to='assetdocuments/')
-    proof_of = models.ForeignKey(Asset, on_delete=models.CASCADE)
+#     proof_of = models.ForeignKey(loanAsset, on_delete=models.CASCADE)
     
-    class Meta:
-        db_table = "asset_document"
+#     class Meta:
+#         db_table = "asset_document"
         
-    def __str__(self):
-        return self.document_name
+#     def __str__(self):
+#         return self.document_name
     
 		
 	
