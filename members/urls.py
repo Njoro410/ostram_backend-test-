edfd8782adda81b1ae9f3762c8f921457e7fa584
sharonkorir <csharonkorir@gmail.com>
2023-02-20 15:30:36 +0300

    Update: change members api endpoints to 'api'

--- members/urls.py
+++ members/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import MemberList, MemberDetail, ResidentialAreaList, ResidentialAreaDetail
 
 urlpatterns = [
-  path('view/', MemberList.as_view()),
-  path('create/', MemberList.as_view()),
-  path('member/<int:pk>/', MemberDetail.as_view()),
+  path('members/view/', MemberList.as_view()),
+  path('members/create/', MemberList.as_view()),
+  path('members/<int:pk>/', MemberDetail.as_view()),
   path('residential-areas/view/', ResidentialAreaList.as_view()),
   path('residential-areas/create/', ResidentialAreaList.as_view()),
   path('residential-areas/<int:pk>/', ResidentialAreaDetail.as_view()),
