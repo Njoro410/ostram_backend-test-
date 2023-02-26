7de2a456dd5480111b4fc12a83e5fbf2e6982e92
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-26 16:43:28 +0300

    created a staff acount that inherits from baseuser

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,8 @@
 from enum import unique
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from administration.choices import *
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 
@@ -35,4 +37,36 @@ class AccountManager(BaseUserManager):
         user.is_superuser = True
         user.save(using=self._db)
         return
- 
+
+
+class staffAccount(AbstractBaseUser):
+    email = models.EmailField(null=False, blank=False, unique=True)
+    username = models.CharField(max_length=50, blank=False, null=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    title = models.CharField(max_length=200, blank=True,
+                             null=True, choices=TITLE_CHOICES)
+    fullname = models.CharField(max_length=200, blank=True, null=True)
+    phone_number = PhoneNumberField(blank=True, null=True)
+    gender = models.CharField(
+        max_length=200, choices=GENDER_CHOICES, blank=True, null=True)
+    dob = models.DateField(blank=True, null=True)
+    current_salary = models.IntegerField(blank=True, null=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
