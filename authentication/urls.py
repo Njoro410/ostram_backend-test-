e87b9d37e07a8f99b6f7b995253b2314a2acc1ca
njoro410 <brian.njoroge@student.moringaschool.com>
2023-02-26 19:52:49 +0300

    added user details url

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login', views.loginView),
     path('register', views.registerView),
-    path('refresh-token', views.refreshTokenView),
+     path('refresh-token', views.CookieTokenRefreshView.as_view()),
     path('logout', views.logoutView),
+    path('user',views.userDetails)
 ]
