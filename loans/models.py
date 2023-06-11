72353fcd356a6eea441fcf9f9186fcf02da5301f
njoro410 <brian.njoroge@student.moringaschool.com>
2023-06-11 06:36:38 +0300

    added new fields to various tables

--- loans/models.py
+++ loans/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from members.models import members
 from administration.choices import *
-from administration.models import baseModel
+from authentication.models import baseModel
 from django.core.exceptions import ValidationError
+from django.conf import settings
 
 
 # Create your models here.
@@ -21,23 +22,36 @@ class Loan_Type(models.Model):
     min_amount_allowed = models.IntegerField(
         null=True, blank=True)
     max_amount_allowed = models.IntegerField(null=True, blank=True)
-    processing_fee = models.DecimalField(
-        max_digits=5, decimal_places=4, null=True, blank=True)
-    insurance_fee = models.DecimalField(
-        max_digits=5, decimal_places=4, null=True, blank=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='loantype_instance_creator', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, blank=True, null=True)
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, blank=True, related_name='loantype_instance_updater', null=True)
+    # processing_fee = models.DecimalField(
+    #     max_digits=5, decimal_places=4, null=True, blank=True)
+    # insurance_fee = models.DecimalField(
+    #     max_digits=5, decimal_places=4, null=True, blank=True)
 
     class Meta:
         db_table = "loan_type"
 
     def __str__(self):
         return self.name
-    
+
+
 class Loan_Status(models.Model):
-    status_name = models.CharField(max_length=60, null=True,blank=True)
-    
+    status_name = models.CharField(max_length=60, null=True, blank=True, choices=LOAN_STATUS_CHOICES)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='loanStatus_instance_creator', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, blank=True, null=True)
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, blank=True, related_name='loanStatus_instance_updater', null=True)
+
     class Meta:
         db_table = "loan_status"
-        
+
     def __str__(self):
         return self.status_name
 
@@ -46,21 +60,19 @@ class Loans(models.Model):
     lendee = models.ForeignKey(
         members, on_delete=models.DO_NOTHING, related_name='borrower')
     loan_type = models.ForeignKey(Loan_Type, on_delete=models.DO_NOTHING)
-    amount = models.DecimalField(max_digits=8, decimal_places=2)
-    status = models.ForeignKey(Loan_Status, on_delete=models.DO_NOTHING)
-    application_date = models.DateField(blank=True,null=True)
-    issue_date = models.DateField(blank=True,null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.ForeignKey(Loan_Status, on_delete=models.PROTECT)
+    application_date = models.DateField(blank=True, null=True)
+    issue_date = models.DateField(blank=True, null=True)
     # repayment_date = models.DateField(auto_now_add=False)
     guarantors = models.ManyToManyField(members, related_name='guarantors')
     payment_frequency = models.CharField(
         max_length=50, choices=PAYMENT_FREQUENCY_CHOICES)
 
-
     def clean(self):
         if self.amount < self.loan_type.min_amount_allowed:
             raise ValidationError(
                 {'amount': f'Amount must be at least {self.loan_type.min_amount_allowed} for the selected loan type.'})
-        
 
     class Meta:
         db_table = "loans"
@@ -71,17 +83,38 @@ class Loans(models.Model):
     @property
     def borrower_membership_number(self):
         return self.lendee.mbr_no
-    
-    
 
 
-class Documents(baseModel):
+class LoanRepayment(models.Model):
+    loan = models.ForeignKey(Loans, on_delete=models.DO_NOTHING, null=True, blank=True)
+    ammount_repaid = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+    loan_interest = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+    date_repaid = models.DateField(blank=True, null=True)
+    loan_form_fee = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+    loan_processing_fee = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+    loan_insurance_fee = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+    late_charges = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+    
+    class Meta:
+        db_table = "loans_repayment"
+    
+    def __str__(self):
+        return f"{self.loan.lendee.names}'s loan repayment"
+
+
+class Documents(models.Model):
     upload_date = models.DateField(auto_now_add=True)
     loan = models.ForeignKey(
         Loans, on_delete=models.CASCADE, null=True, related_name='loan_documents')
     document_type = models.ForeignKey(
         'documentType', on_delete=models.CASCADE, null=True)
     file = models.FileField(upload_to='documents/')
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='documents_instance_creator', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, blank=True, null=True)
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, blank=True, related_name='documents_instance_updater', null=True)
 
     class Meta:
         db_table = "documents"
@@ -90,9 +123,15 @@ class Documents(baseModel):
         return f" {self.document_type.name}"
 
 
-class documentType(baseModel):
+class documentType(models.Model):
     name = models.CharField(max_length=50, choices=DOCUMENT_TYPE_CHOICES)
     description = models.TextField()
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='documentType_instance_creator', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, blank=True, null=True)
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, blank=True, related_name='documentType_instance_updater', null=True)
 
     class Meta:
         db_table = "document_type"
--- savings/models.py
+++ savings/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 from members.models import members
-from administration.models import baseModel
-
+from authentication.models import baseModel
+from django.conf import settings
 # Create your models here.
-class Savings_Account(baseModel):
-    account_owner = models.ForeignKey(members, on_delete=models.CASCADE, related_name='savings_account')  
-    balance = models.DecimalField(max_digits=8, decimal_places=3)
-    
+
+
+class Savings_Account(models.Model):
+    account_owner = models.ForeignKey(
+        members, on_delete=models.CASCADE, related_name='savings_account')
+    savings_balance = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='savings_instance_creator', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, blank=True, null=True)
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, blank=True, related_name='savings_instance_updater', null=True)
+
     class Meta:
         db_table = "savings_account"
-    
+
     def __str__(self):
         return self.account_owner.names
-    
