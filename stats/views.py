29f2a08798a0d1e440bdd7a9df736c5fd673b631
njoro410 <brian.njoroge@student.moringaschool.com>
2023-06-11 06:35:51 +0300

    added a view func to get total savings for the current year

---
+++ stats/views.py
@@ -0,0 +1,22 @@
+from rest_framework.decorators import api_view
+from savings.models import Savings_Account
+from rest_framework.response import Response
+from django.db.models import Sum
+from datetime import datetime
+from rest_framework import status
+# Create your views here.
+
+
+@api_view(['GET'])
+def get_total_savings_currentYear(request):
+    try:
+        current_year = datetime.now().year
+        total_savings = Savings_Account.objects.filter(
+        created_on__year=current_year).aggregate(sum=Sum('savings_balance'))['sum']
+    except Savings_Account.DoesNotExist:
+        return Response({'Error': 'There are no members'}, status=status.HTTP_404_NOT_FOUND)
+
+    if total_savings is None:
+        total_savings = 0.0
+
+    return Response({f'total_savings_{current_year}': total_savings}, status=status.HTTP_200_OK)
