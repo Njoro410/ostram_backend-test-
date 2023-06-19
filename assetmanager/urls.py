6e3271616059cebf98dc38018f310b3eab218ecb
njoro410 <brian.njoroge@student.moringaschool.com>
2023-06-19 23:56:59 +0300

    added assetmanager urls

--- assetmanager/urls.py
+++ assetmanager/urls.py
@@ -4,5 +4,7 @@ from .import views
 
 urlpatterns = [
     path('loan_assets/<int:id>/',views.loan_asset_by_loan_id,name='loan_assets'),
-    path('loan_asset_document/<int:id>/',views.specific_loan_asset,name='loan_asset_document'),
+    path('loan_assets/',views.loan_asset_by_loan_id,name='loan_assets_without_id'),
+    path('loan_asset_document/<int:loan_asset_id>/',views.loan_asset_documents,name='loan_asset_document'),
+    path('loan_asset_document/',views.loan_asset_documents,name='loan_asset_document_without_id'),
 ]
